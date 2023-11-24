397673a9cdda762cee48dd3faff17f24a727495d
mrdltn <mrdltn@hotmail.com>
2023-11-24 15:32:48 +0700

    added Provider component and BASE_URL

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://restcountries.com/v3.1/';
+
+export const ALL_COUNTRIES =
+    BASE_URL + 'all?fields=name,capital,flags,population,region';
+
+export const searchByCountry = (name) => BASE_URL + 'name/' + name;
+
+export const filterByCode = (codes) =>
+    BASE_URL + 'apha?codes=' + codes.join(',');
